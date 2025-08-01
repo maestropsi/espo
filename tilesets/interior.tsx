ae299e09fdc4da4c17fd71888faf6eaecefb0dc5
maestropsi <maestro@determina.me>
2025-08-01 19:08:39 -0300

    Atualização do mapa principal

---
+++ tilesets/interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="interior" tilewidth="32" tileheight="32" tilecount="8025" columns="25">
+ <image source="interior.png" width="800" height="10272"/>
+</tileset>
